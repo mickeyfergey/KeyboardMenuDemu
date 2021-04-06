cba9b427bafd35b656b89b403b9061b9c2dafb52
mickeyfergey <65882924+mickeyfergey@users.noreply.github.com>
2021-04-06 09:41:37 -0500

    Add files via upload

---
+++ KeyboardMenuDemo/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using static System.Console;
+namespace KeyboardMenuDemo
+{
+    public class Game
+    {
+
+        public void Start()
+        {
+            Title = "Menu Generator";
+
+            newMenu();
+        }
+
+        // Here's all the logic you need to work with!
+
+        private void newMenu()
+        {
+            string prompt = "Entry prompt goes here";
+            string[] options = { "Type", "each", "option", "here", "seprately" };
+            Menu newMenu = new Menu(prompt, options);
+            int selectedIndex = newMenu.Run();
+            WriteLine($"Your selected option is {options[selectedIndex]}, and your option number is {selectedIndex + 1}!");
+        }
+    }
+}
---
+++ KeyboardMenuDemo/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace KeyboardMenuDemo
+{
+    class Program
+    {
+        static void Main()
+        {
+            Game game = new Game();
+            game.Start();
+        }
+    }
+}
